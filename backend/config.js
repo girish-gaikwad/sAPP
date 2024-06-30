f686aaaa4012b580ec993000af17c1f3697e965d
Girish_Gaikwad <145460775+girish-gaikwad@users.noreply.github.com>
2024-06-30 20:11:13 +0530

    dep's added

---
+++ backend/config.js
@@ -0,0 +1,10 @@
+// config.js
+
+const ENV = {
+    // DATABASE_URL: mongodb+srv://girishgaikwad2055:9842123@chatapp.yo3eo5w.mongodb.net/chatapp ,
+    JWT_SECRET : 'ijPvUuEdiqKtiBRkHM/UuvBn5ZKGHTbYIAk0LGcfB9o=',
+    EMAIL: 'vibez2055@gmail.com',  // Replace with your Gmail email address
+    PASSWORD: 'vibez16062006'     // Replace with your Gmail app password
+};
+
+export default ENV;
---
+++ backend/index.js
@@ -0,0 +1,47 @@
+import connect from "./database/conn.js";
+// initializing the server and connecting to the database,
+import express from "express";
+import router from "./routes/routes.js";
+import cors from "cors";
+
+
+import bodyParser from 'body-parser';
+
+// const app = express();
+
+// parse application/json
+
+const app = express();
+app.use(bodyParser.json());
+
+// Define routes and middleware here
+
+// listen the changes of this file on port 3000
+app.use(express.json());
+
+const port = 5001;
+
+// Enable CORS for all routes
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.status(201).json("Home Get Request");
+});
+
+/** api routes */
+app.use("/api", router);
+
+/** start server only when we have valid connection */
+connect()
+  .then(() => {
+    try {
+      app.listen(port, () => {
+        console.log(`Server connected to http://localhost:${port}`);
+      });
+    } catch (error) {
+      console.log("Cannot connect to the server");
+    }
+  })
+  .catch((error) => {
+    console.log("Invalid database connection...!");
+  });
