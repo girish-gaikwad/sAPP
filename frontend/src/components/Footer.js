14c421935cdff8882a524281bca5ee391ebb4ea6
Girish_Gaikwad <145460775+girish-gaikwad@users.noreply.github.com>
2024-06-30 20:08:17 +0530

    updated componenets

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../styles/Footer.scss';
+
+const Footer = () => {
+   return (
+      <footer className="footer">
+         <div className="footer__wrapper container">
+            <div className="text--center">
+               <p>Done with contribution of open source and team tech bloomers</p>
+            </div>
+         </div>
+      </footer>
+   );
+};
+
+export default Footer;
---
+++ frontend/src/components/chart.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { Chart } from 'react-google-charts';
+
+const EmotionLineChart = () => {
+  const data = [
+    ['Day', 'Joy', 'Surprise', 'Anger', 'Sad', 'Happy'],
+    ['Monday', 20, 15, 10, 5, 8],
+    ['Tuesday', 18, 13, 11, 7, 10],
+    ['Wednesday', 22, 17, 9, 4, 6],
+    ['Tuesday', 25, 12, 15, 6, 9],
+    ['Friday', 16, 20, 11, 8, 12],
+    ['Saturday', 19, 18, 8, 7, 11],
+    ['Sunday', 23, 16, 13, 5, 9]
+  ];
+
+  return (
+    <div className="chart-container" style={{ width: '100%', maxWidth: 800 }}>
+      <Chart
+        width={'100%'}
+        height={400}
+        chartType="LineChart"
+        loader={<div>Loading Chart</div>}
+        data={data}
+        options={{
+          title: '',
+          hAxis: {
+            title: 'Months',
+          },
+          vAxis: {
+            title: 'Emotion Frequency',
+          },
+          tooltip: { isHtml: true }, // Enable HTML content in tooltip
+        }}
+      />
+    </div>
+  );
+};
+
+export default EmotionLineChart;
